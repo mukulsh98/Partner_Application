de294990467416c70978ad0dcb965886724e8f2f
pawanharariya <pawanharariya@gmail.com>
2020-04-16 14:28:27 +0530

    partner wallet added

--- app/src/main/java/com/example/prototype_1/my_account_fragment.java
+++ app/src/main/java/com/example/prototype_1/my_account_fragment.java
@@ -27,16 +27,17 @@ import java.util.ArrayList;
 
 public class my_account_fragment extends Fragment {
 
-    private ImageView location,payment,discount,qrcode;
+    private ImageView location, payment, discount, qrcode, wallletBtn;
     private Switch profile_status;
     final int UPI_PAYMENT = 0;
     private PaymentsClient paymentsClient;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v= inflater.inflate(R.layout.fragment_my_account,container,false);
+        View v = inflater.inflate(R.layout.fragment_my_account, container, false);
 
-        location= (ImageView)v.findViewById(R.id.location);
+        location = (ImageView) v.findViewById(R.id.location);
         location.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -44,7 +45,15 @@ public class my_account_fragment extends Fragment {
 
             }
         });
-        payment= (ImageView) v.findViewById(R.id.payment);
+        wallletBtn = v.findViewById(R.id.walletBtn);
+        wallletBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getContext(),Wallet.class);
+                startActivity(i);
+            }
+        });
+        payment = (ImageView) v.findViewById(R.id.payment);
         payment.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -52,33 +61,32 @@ public class my_account_fragment extends Fragment {
             }
         });
 
-        discount=(ImageView)v.findViewById(R.id.discout);
+        discount = (ImageView) v.findViewById(R.id.discout);
         discount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                startActivity(new Intent(getActivity(),discount.class));
+                startActivity(new Intent(getActivity(), discount.class));
             }
         });
 
-        qrcode=(ImageView)v.findViewById(R.id.qrcode);
+        qrcode = (ImageView) v.findViewById(R.id.qrcode);
         qrcode.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
             }
         });
-        profile_status=(Switch)v.findViewById(R.id.profile);
+        profile_status = (Switch) v.findViewById(R.id.profile);
         profile_status.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked==true){
+                if (isChecked == true) {
 
-                }
-                else{
+                } else {
                     // Change the status of partner to off...
 
-                    Toast.makeText(getActivity(),"Status Unactive",Toast.LENGTH_LONG).show();
+                    Toast.makeText(getActivity(), "Status Unactive", Toast.LENGTH_LONG).show();
                 }
             }
         });
@@ -86,14 +94,14 @@ public class my_account_fragment extends Fragment {
         return v;
     }
 
-    public void payUsingUpi(){
+    public void payUsingUpi() {
 
-        String name="";
-        String upiId="";
-        String note="";
-        String amount="";
+        String name = "";
+        String upiId = "";
+        String note = "";
+        String amount = "";
 
-        Log.e("main ", "name "+name +"--up--"+upiId+"--"+ note+"--"+amount);
+        Log.e("main ", "name " + name + "--up--" + upiId + "--" + note + "--" + amount);
 
         Uri uri = Uri.parse("upi://pay").buildUpon()
                 .appendQueryParameter("pa", upiId)
@@ -111,10 +119,10 @@ public class my_account_fragment extends Fragment {
         // will always show a dialog to user to choose an app
         Intent chooser = Intent.createChooser(upiPayIntent, "Pay with");
         // check if intent resolves
-        if(null != chooser.resolveActivity(getActivity().getPackageManager())) {
+        if (null != chooser.resolveActivity(getActivity().getPackageManager())) {
             startActivityForResult(chooser, UPI_PAYMENT);
         } else {
-            Toast.makeText(getActivity(),"No UPI app found, please install one to continue",Toast.LENGTH_SHORT).show();
+            Toast.makeText(getActivity(), "No UPI app found, please install one to continue", Toast.LENGTH_SHORT).show();
         }
 
 
@@ -123,38 +131,34 @@ public class my_account_fragment extends Fragment {
     private void upiPaymentDataOperation(ArrayList<String> data) {
         if (isConnectionAvailable(getActivity())) {
             String str = data.get(0);
-            Log.e("UPIPAY", "upiPaymentDataOperation: "+str);
+            Log.e("UPIPAY", "upiPaymentDataOperation: " + str);
             String paymentCancel = "";
-            if(str == null) str = "discard";
+            if (str == null) str = "discard";
             String status = "";
             String approvalRefNo = "";
             String response[] = str.split("&");
             for (int i = 0; i < response.length; i++) {
                 String equalStr[] = response[i].split("=");
-                if(equalStr.length >= 2) {
+                if (equalStr.length >= 2) {
                     if (equalStr[0].toLowerCase().equals("Status".toLowerCase())) {
                         status = equalStr[1].toLowerCase();
-                    }
-                    else if (equalStr[0].toLowerCase().equals("ApprovalRefNo".toLowerCase()) || equalStr[0].toLowerCase().equals("txnRef".toLowerCase())) {
+                    } else if (equalStr[0].toLowerCase().equals("ApprovalRefNo".toLowerCase()) || equalStr[0].toLowerCase().equals("txnRef".toLowerCase())) {
                         approvalRefNo = equalStr[1];
                     }
-                }
-                else {
+                } else {
                     paymentCancel = "Payment cancelled by user.";
                 }
             }
             if (status.equals("success")) {
                 //Code to handle successful transaction here.
                 Toast.makeText(getActivity(), "Transaction successful.", Toast.LENGTH_SHORT).show();
-                Log.e("UPI", "payment successfull: "+approvalRefNo);
-            }
-            else if("Payment cancelled by user.".equals(paymentCancel)) {
+                Log.e("UPI", "payment successfull: " + approvalRefNo);
+            } else if ("Payment cancelled by user.".equals(paymentCancel)) {
                 Toast.makeText(getActivity(), "Payment cancelled by user.", Toast.LENGTH_SHORT).show();
-                Log.e("UPI", "Cancelled by user: "+approvalRefNo);
-            }
-            else {
+                Log.e("UPI", "Cancelled by user: " + approvalRefNo);
+            } else {
                 Toast.makeText(getActivity(), "Transaction failed.Please try again", Toast.LENGTH_SHORT).show();
-                Log.e("UPI", "failed payment: "+approvalRefNo);
+                Log.e("UPI", "failed payment: " + approvalRefNo);
             }
         } else {
             Log.e("UPI", "Internet issue: ");
@@ -178,12 +182,12 @@ public class my_account_fragment extends Fragment {
 
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
 
-        IntentResult res= IntentIntegrator.parseActivityResult(requestCode,resultCode,data);
+        IntentResult res = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
 
-        if(res!=null && res.getContents()!=null){
+        if (res != null && res.getContents() != null) {
             // the result from the scanned image is saved in this string...
-            String result=res.getContents();
-            Toast.makeText(getActivity(),result,Toast.LENGTH_LONG).show();
+            String result = res.getContents();
+            Toast.makeText(getActivity(), result, Toast.LENGTH_LONG).show();
 
         }
 
